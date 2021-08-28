f35bb35fd28129101eb144e043b3e82047a3670b
green-maple <80803474+green-maple@users.noreply.github.com>
2021-08-29 00:12:49 +0530

    Add files via upload
    Write a function to reverse an input string with following requirements: 
    - should be inline
    - without using any built-in functions

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class ReverseString {
+
+    public String reverseStr(String str) {
+        char[] ch = str.toCharArray();
+        int right = str.length() - 1;
+        int len = ((right % 2) == 0) ? right/2-1 : right/2+1;
+        for (int left = 0; left < len; left++, right--) {
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+        }
+        return String.copyValueOf(ch);
+    }
+
+    @Test
+    public void testReverseStr() {
+        String str = "hello world";
+        System.out.println("input string: " + str);
+        System.out.println("reversed string: " + reverseStr(str));
+        System.out.println("get back original input string: " + reverseStr(reverseStr(str)));
+    }
+}
